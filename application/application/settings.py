37cb7413ad3d0e126e7e97cc469761064a5ab942
Ashley Osborne <ashley.osborne@prs-im.co.uk>
2025-11-24 16:40:11 +0000

    Switch to CompressedStaticFilesStorage and regenerate staticfiles

--- application/application/settings.py
+++ application/application/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
